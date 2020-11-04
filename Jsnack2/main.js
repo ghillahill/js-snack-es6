afc2f1e9161884525899226000c7aa15d97d9384
Alberto <thebertsinfo@gmail.com>
2020-11-04 16:50:44 +0100

    Jsnack2: Creato Array Oggetti e test filter

---
+++ Jsnack2/main.js
@@ -0,0 +1,53 @@
+// Partendo da un array creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri
+// inseriti dall'utente
+
+//Usare ES6
+
+$(document).ready(function() {
+
+    var listaNomi = [
+        {
+            nome:'Alberto',
+        },
+        {
+            nome:'Chiara',
+        },
+        {
+            nome:'Simone',
+        },
+        {
+            nome:'Carlo',
+        },
+        {
+            nome:'Davide',
+        },
+        {
+            nome:'Erica',
+        },
+        {
+            nome:'Carla',
+        },
+        {
+            nome:'Filippo',
+        },
+        {
+            nome:'Maria',
+        },
+        {
+            nome:'Sandro',
+        },
+    ];
+
+    /*
+let newListaNomi = listaNomi.filter((sceltauno, sceltaDue) => {
+        sceltaUno = parseInt(prompt("Digitare un numero da 0 a 9"));
+        sceltaDue = parseInt(prompt("Digitare un numero da 0 a 9"));
+        return sceltaUno && sceltaDue;
+    });*/
+
+
+
+    console.log(newListaNomi);
+
+
+});
